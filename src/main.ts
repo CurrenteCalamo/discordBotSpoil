434b315622418ccdae52eb42f18d80832699cffd
George Khrulev <pppp43777@gmail.com>
2023-01-02 02:31:26 +0300

    chore: created client for discord bot

---
+++ src/main.ts
@@ -0,0 +1,25 @@
+import { Client, GatewayIntentBits } from 'discord.js'
+import { Events, ActivityType } from 'discord.js'
+import * as dotenv from 'dotenv'
+dotenv.config()
+
+const client = new Client({
+	intents: [
+		GatewayIntentBits.Guilds,
+	],
+})
+
+client.on(Events.ShardReady, () => {
+	console.log(`Logged in as ${client.user?.tag}!`)
+	client.user?.setStatus('dnd')
+	client.user?.setActivity(
+        "⠤⢿⡄⠹⣧⣷⣸⡇⠄⠄⠲⢰⣌⣾⣿⣿⣿⣿⣿⣿⣶⣤⣤⡀⠄⠈⠻⢮\
+        ⠄⢸⣧⠄⢘⢻⣿⡇⢀⣀⠄⣸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣧⡀⠄⢀\
+        ⠄⠈⣿⡆⢸⣿⣿⣿⣬⣭⣴⣿⣿⣿⣿⣿⣿⣿⣯⠝⠛⠛⠙⢿⡿⠃⠄⢸",
+		{
+		    type: ActivityType.Playing,
+		},
+	)
+})
+
+client.login(process.env.TOKEN)

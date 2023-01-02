1fdd9c9eec0dfbe625ed444bfffff7e4a7a80c69
George Khrulev <pppp43777@gmail.com>
2023-01-03 02:15:54 +0300

    chore: change configuration of prettier

--- src/main.ts
+++ src/main.ts
@@ -1,25 +1,23 @@
-import { Client, GatewayIntentBits } from 'discord.js'
-import { Events, ActivityType } from 'discord.js'
-import * as dotenv from 'dotenv'
-dotenv.config()
+import { Client, GatewayIntentBits } from 'discord.js';
+import { Events, ActivityType } from 'discord.js';
+import * as dotenv from 'dotenv';
+dotenv.config();
 
 const client = new Client({
-	intents: [
-		GatewayIntentBits.Guilds,
-	],
-})
+  intents: [GatewayIntentBits.Guilds],
+});
 
 client.on(Events.ShardReady, () => {
-	console.log(`Logged in as ${client.user?.tag}!`)
-	client.user?.setStatus('dnd')
-	client.user?.setActivity(
-        "⠤⢿⡄⠹⣧⣷⣸⡇⠄⠄⠲⢰⣌⣾⣿⣿⣿⣿⣿⣿⣶⣤⣤⡀⠄⠈⠻⢮\
+  console.log(`Logged in as ${client.user?.tag}!`);
+  client.user?.setStatus('dnd');
+  client.user?.setActivity(
+    '⠤⢿⡄⠹⣧⣷⣸⡇⠄⠄⠲⢰⣌⣾⣿⣿⣿⣿⣿⣿⣶⣤⣤⡀⠄⠈⠻⢮\
         ⠄⢸⣧⠄⢘⢻⣿⡇⢀⣀⠄⣸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣧⡀⠄⢀\
-        ⠄⠈⣿⡆⢸⣿⣿⣿⣬⣭⣴⣿⣿⣿⣿⣿⣿⣿⣯⠝⠛⠛⠙⢿⡿⠃⠄⢸",
-		{
-		    type: ActivityType.Playing,
-		},
-	)
-})
+        ⠄⠈⣿⡆⢸⣿⣿⣿⣬⣭⣴⣿⣿⣿⣿⣿⣿⣿⣯⠝⠛⠛⠙⢿⡿⠃⠄⢸',
+    {
+      type: ActivityType.Playing,
+    },
+  );
+});
 
-client.login(process.env.TOKEN)
+client.login(process.env.TOKEN);

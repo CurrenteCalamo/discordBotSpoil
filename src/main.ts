590536176c8268a097280ad97e3f0abc69cc72fb
George Khrulev <pppp43777@gmail.com>
2023-01-04 03:18:39 +0300

    chore: added InteractionCreate event handler && type for it

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,46 @@
-import { Client, GatewayIntentBits } from 'discord.js';
-import { Events, ActivityType } from 'discord.js';
-import * as dotenv from 'dotenv';
-dotenv.config();
+import { Client, Collection, GatewayIntentBits } from 'discord.js'
+import { SlashCommandInterface } from './types/index.js'
+import { Events, ActivityType } from 'discord.js'
+import { fileURLToPath } from 'url'
+import * as dotenv from 'dotenv'
+import * as path from 'path'
+import * as fs from 'fs'
+
+dotenv.config()
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
 
 const client = new Client({
-  intents: [GatewayIntentBits.Guilds],
-});
+  intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildIntegrations],
+})
+
+const commands = new Collection<string, SlashCommandInterface>()
+const commandFiles = fs
+  .readdirSync(path.join(__dirname, './commands'))
+  .filter((file) => file.endsWith('.js'))
+for (const file of commandFiles) {
+  const command = await import(path.join(__dirname, `./commands/${file}`))
+  commands.set(command.default.data.name, command.default)
+}
+
+client.on(Events.InteractionCreate, async (interaction) => {
+  if (!interaction.isCommand()) return
+  const command = commands.get(interaction.commandName)
+  try {
+    command?.execute(interaction, client)
+  } catch (error) {
+    console.error(error)
+    await interaction.reply({
+      content: 'There was an error while executing this command!',
+      ephemeral: true,
+    })
+  }
+})
 
 client.on(Events.ShardReady, () => {
-  console.log(`Logged in as ${client.user?.tag}!`);
-  client.user?.setStatus('dnd');
+  console.log(`Logged in as ${client.user?.tag}!`)
+  client.user?.setStatus('dnd')
   client.user?.setActivity(
     '⠤⢿⡄⠹⣧⣷⣸⡇⠄⠄⠲⢰⣌⣾⣿⣿⣿⣿⣿⣿⣶⣤⣤⡀⠄⠈⠻⢮\
         ⠄⢸⣧⠄⢘⢻⣿⡇⢀⣀⠄⣸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣧⡀⠄⢀\
@@ -17,7 +48,7 @@ client.on(Events.ShardReady, () => {
     {
       type: ActivityType.Playing,
     },
-  );
-});
+  )
+})
 
-client.login(process.env.TOKEN);
+client.login(process.env.TOKEN)

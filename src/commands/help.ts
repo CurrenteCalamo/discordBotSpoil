38894c0264ddec8abaf1aedfd6c7c063d1050e9a
George Khrulev <pppp43777@gmail.com>
2023-01-05 01:21:35 +0300

    chore: created help command

--- src/commands/help.ts
+++ src/commands/help.ts
@@ -1,5 +1,14 @@
 import { SlashCommandBuilder } from '@discordjs/builders'
 import { CommandInteraction } from 'discord.js'
+import { fileURLToPath } from 'url'
+import * as dotenv from 'dotenv'
+import * as path from 'path'
+import * as fs from 'fs'
+import { SlashCommandInterface } from '../types/index.js'
+dotenv.config()
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
 
 export default {
   data: new SlashCommandBuilder()
@@ -10,8 +19,26 @@ export default {
       ru: 'Получить страницу справки сервера.',
     }),
   async execute(interaction: CommandInteraction) {
+    let content = '**Server help page:**\n'
+
+    const commandFiles = fs
+      .readdirSync(path.join(__dirname))
+      .filter((file) => file.endsWith('.js'))
+    for (const file of commandFiles) {
+      const command = await import(path.join(__dirname, `./${file}`))
+      const options = command.default.data?.options.map(
+        (i: SlashCommandBuilder) => ` \`${i.name}\``,
+      )
+
+      const description =
+        command.default.data?.description_localizations[interaction.locale] ||
+        command.default.data?.description
+      content += `- \`/${command.default.data?.name}\`${options} — ${description}\n`
+    }
+
     return await interaction.reply({
-      content: 'help',
+      content,
+      ephemeral: true,
     })
   },
 }

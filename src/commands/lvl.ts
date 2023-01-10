29f1e6778d73da4b60c21317b93588fc26f3cdb8
George Khrulev <pppp43777@gmail.com>
2023-01-11 01:52:29 +0300

    chore: reworked lvl command

--- src/commands/lvl.ts
+++ src/commands/lvl.ts
@@ -1,5 +1,6 @@
 import { SlashCommandBuilder } from '@discordjs/builders'
-import { CommandInteraction } from 'discord.js'
+import { CommandInteraction, EmbedBuilder } from 'discord.js'
+import User from '../models/User.js'
 
 export default {
   data: new SlashCommandBuilder()
@@ -10,8 +11,33 @@ export default {
       ru: 'Показать мой текущий уровень.',
     }),
   async execute(interaction: CommandInteraction) {
+    let user = await User.findOne({
+      userId: interaction.user.id,
+      guildId: interaction.guild?.id,
+    })
+    if (!user)
+      user = await User.create({
+        userId: interaction.user.id,
+        guildId: interaction.guild?.id,
+      })
+
+    const Embed = new EmbedBuilder()
+      .setTitle(`Current level is ${interaction.user.username}`)
+      .setThumbnail(interaction.user.displayAvatarURL({ forceStatic: true }))
+      .addFields(
+        {
+          name: 'Level:',
+          value: `\`\`\`${user.lvl}\`\`\``,
+          inline: true,
+        },
+        {
+          name: 'Experience:',
+          value: `\`\`\`${user.xp}/30\`\`\``,
+          inline: true,
+        },
+      )
     return await interaction.reply({
-      content: 'lvl',
+      embeds: [Embed],
     })
   },
 }
